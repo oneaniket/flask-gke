5a8d6c3734cdb10bc4c1f1328fad2478a2330183
oneaniket <50626458+oneaniket@users.noreply.github.com>
2023-08-25 22:46:40 +0530

    Update app.py
    hello flask

--- flask/app.py
+++ flask/app.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("ATC_USERNAME="+os.environ.get("ATC_USERNAME")+"\nATC_PASSWORD="+os.environ.get("ATC_PASSWORD"))
-    #return "Username="+str(os.environ.get("ATC_USERNAME "))+"\nPassword="+str(os.environ.get("ATC_PASSWORD\n"))
-    return'<p>' + os.environ.get("ATC_USERNAME") + '</p> <p>'+ os.environ.get("ATC_PASSWORD") + '</p>'
+    return 'Web App with Python Flask!'
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
