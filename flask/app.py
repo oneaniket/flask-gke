a606322343858c9944983c791b4a8b79c4a6ba17
oneaniket <50626458+oneaniket@users.noreply.github.com>
2023-08-26 12:22:08 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'Web App with Python Flask'
+    return 'welcome Ruia Devops'
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
